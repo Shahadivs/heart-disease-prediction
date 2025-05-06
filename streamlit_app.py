40b09ac46a81ab0da2f8b54a7febbc6bd2714a7f
Shahadivs <444008729@pnu.edu.sa>
2025-05-06 18:57:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,17 @@
 import streamlit as st
-import pickle
 import numpy as np
+import pickle
 import pandas as pd
-
 
 # تحميل الموديل والسكيلر
 model = pickle.load(open('svc_model.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
 
-
+# دالة التصنيف
 def classify(prediction):
     return 'Has Heart Disease' if prediction == 1 else 'No Heart Disease'
 
+# الواجهة الرئيسية
 def main():
     st.title("Heart Disease Prediction App")
     html_temp = """
@@ -21,16 +21,7 @@ def main():
     """
     st.markdown(html_temp, unsafe_allow_html=True)
 
-def main():
-    st.title("Heart Disease Prediction App")
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Heart Disease Prediction using SVM</h2>
-    </div>
-    """
-    st.markdown(html_temp, unsafe_allow_html=True)
-
-    # المدخلات من المستخدم (كلها لازم تكون داخـل الدالة ومزحومة)
+    # الحقول المدخلة من المستخدم (نفس 11 اللي تدرب عليهم الموديل)
     age = st.slider('Age', 20, 100)
     sex = st.selectbox('Sex (1=Male, 0=Female)', [1, 0])
     cp = st.slider('Chest Pain Type (0-3)', 0, 3)
@@ -41,20 +32,16 @@ def main():
     thalachh = st.slider('Max Heart Rate Achieved', 70, 210)
     exng = st.selectbox('Exercise Induced Angina (1=Yes, 0=No)', [1, 0])
     oldpeak = st.slider('Oldpeak (ST depression)', 0.0, 6.0)
-    slp = st.slider('Slope of the peak exercise ST segment', 0, 2)
-    caa = st.slider('Number of major vessels (0-4)', 0, 4)
     thall = st.slider('Thalassemia (0=Normal, 1=Fixed Defect, 2=Reversible Defect)', 0, 2)
 
-# تجهيز البيانات للتنبؤ
+    # التجهيز للتنبؤ
     features = np.array([[age, sex, cp, trtbps, chol, fbs, restecg,
-    thalachh, exng, oldpeak, slp, caa, thall]])
+                          thalachh, exng, oldpeak, thall]])
     scaled_features = scaler.transform(features)
 
     if st.button('Predict'):
         prediction = model.predict(scaled_features)[0]
         st.success(f'Result: {classify(prediction)}')
 
-
-
-
-
+if __name__ == '__main__':
+    main()
